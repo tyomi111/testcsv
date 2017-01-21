06a03c510981973a3f33d5699842995571639a71
tyomi <tyomi111@gmail.com>
2017-01-21 11:45:31 +0900

    first commit.

---
+++ testcsv.ps1
@@ -0,0 +1,5 @@
+﻿get-childItem "*.csv" | foreach {
+ Import-Csv -Path $_ | Select ITEM | Export-Csv -Append -NoTypeInformation -Path AB.csv
+}
+
+Import-csv AB.csv | foreach { $_.ITEM } | Sort-Object -Unique
